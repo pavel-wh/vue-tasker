ac58d25c088a4852e4cecf577d933a7ccc4e34fc
pB <webforeman@gmail.com>
2020-06-07 00:35:56 +0300

    Created store and localstorage for new task

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,20 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-	state: {},
-	mutations: {},
-	actions: {},
+	state: {
+		tasks: JSON.parse(localStorage.getItem('tasks') || '[]')
+	},
+	mutations: {
+		createTask(state, task) {
+			state.tasks.push(task)
+
+			localStorage.setItem('task', JSON.stringify(state.tasks))
+		}
+	},
+	actions: {
+		createTask({commit}, task) {
+			commit('createTask', task)
+		}
+	},
 	modules: {},
 })
--- src/views/Create.vue
+++ src/views/Create.vue
@@ -2,12 +2,24 @@
 	<div class="row">
 		<div class="col s6 offset-s3">
 			<h1>Create task</h1>
-			<form action="">
+			<form @submit.prevent="createTask">
 				<div class="input-field">
-					<input type="text" name="title" id="title" class="validate" required placeholder="Title">
-					<label for="title"></label>
+					<input type="text" name="title" id="title" class="validate" required v-model="title">
+					<label for="title">Title</label>
+          <span class="helper-text" data-error="Title is required"></span>
 				</div>
-				<div class="chips" ref="chips"></div>
+				<div class="input-field">
+					<div class="chips" ref="chips"></div>
+				</div>
+				<div class="input-field">
+					<textarea id="description" class="materialize-textarea"  data-length="2048" height="400" v-model="description"></textarea>
+					<label for="description">Description</label>
+				</div>
+				<div class="input-field">
+					<input type="text" id="deadline" ref="datepicker">
+					<label for="description">Deadline</label>
+				</div>
+				<button class="waves-effect waves-light btn" type="submit">Create Task</button>
 			</form>
 		</div>
 	</div>
@@ -15,14 +27,54 @@
  
 <script>
 import M from 'materialize-css'
+import { v4 as uuidv4 } from 'uuid';
 export default {
 	name: 'Create',
+	data() {
+		return {
+			description: '',
+			title: '',
+			cheaps: null,
+			date: null
+		}
+	},
 	components: {},
 	mounted() {
-		M.AutoInit()
-		M.Chips.init(this.$refs.chips, {
+
+		const textNeedCount = document.querySelector('#description')
+		textNeedCount.style.height = '150px'
+		new M.CharacterCounter(textNeedCount)
+
+		this.chips = M.Chips.init(this.$refs.chips, {
 			placeholder: 'Task tags'
 		});
+
+		this.date = M.Datepicker.init(this.$refs.datepicker, {
+			format: 'dd.mm.yyyy',
+			defaultDate: new Date(),
+			setDefaultDate: true
+		})
+	},
+	methods: {
+		createTask() {
+			const task = {
+				title: this.title,
+				description: this.description,
+				id: uuidv4(),
+				status: 'active',
+				tags: this.chips.chipsData,
+				date: this.date.date
+			}
+			this.$store.dispatch('createTask', task)
+		}
+	},
+	destroyed() {
+		if (this.date && this.date.destroy) {
+			this.date.destroy()
+		}
+		if (this.chips && this.chips.destroy) {
+			this.chips.destroy()
+		}
 	}
 }
 </script>
